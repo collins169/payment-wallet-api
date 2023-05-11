52fa59f457d5a221fafa40432ee9e494ad5d8965
Onyetube Kosisochukwu <kosisochukwu@cbitindustries.com>
2023-05-11 21:47:02 +0000

    setup project structure

---
+++ src/account/service/auth.service.ts
@@ -0,0 +1,88 @@
+import { compareSync, genSaltSync, hashSync } from 'bcrypt';
+import { JwtPayload, sign, verify } from 'jsonwebtoken';
+import { isEmpty } from 'lodash';
+import { ErrorHandler } from '../../common/helpers/errorHandler';
+import { HttpStatus } from '../../common/types';
+import { AuthResponse, CreateAccountInput } from '../../common/types/types';
+import Account from '../entities/account.entity';
+import { getAccountById } from './account.service';
+
+export const registerAccount = async (input: CreateAccountInput) => {
+	const existingAccount = await Account.findOne({
+		email: input.email,
+	});
+	if (!isEmpty(existingAccount)) {
+		throw new ErrorHandler(
+			'Account with email already exist',
+			HttpStatus.CONFLICT
+		);
+	}
+	const salt = genSaltSync(10);
+	const hashPassword = hashSync(input.password, salt);
+	const acct = new Account({ ...input, password: hashPassword });
+	const account = await acct.save();
+
+	return {
+		id: account.id,
+		name: account.name,
+		email: account.email,
+	};
+};
+
+export const authenticate = async (
+	email: string,
+	password: string
+): Promise<AuthResponse> => {
+	const account = await Account.findOne({
+		email,
+	});
+
+	const errorMessage = 'Invalid username/password';
+	if (isEmpty(account)) {
+		throw new ErrorHandler(errorMessage, HttpStatus.UNAUTHORIZED);
+	}
+
+	//Validate user password
+	const match = compareSync(password, account.password);
+
+	if (!match) {
+		throw new ErrorHandler(errorMessage, HttpStatus.UNAUTHORIZED);
+	}
+
+	//Generate jwt token
+	const token = sign(
+		{ id: account.id },
+		process.env.SECRET_KEY || 'payment-wallet-api',
+		{
+			algorithm: 'HS256',
+			expiresIn: '1d',
+			subject: account.id,
+		}
+	);
+
+	return {
+		token,
+		account: {
+			id: account.id,
+			name: account.name,
+			email: account.email,
+		},
+	};
+};
+
+export const validateToken = async (token: string) => {
+	if (isEmpty(token)) {
+		return {};
+	}
+	const { sub: accountId } = verify(
+		token,
+		process.env.SECRET_KEY || 'payment-wallet-api'
+	) as JwtPayload;
+
+	if (isEmpty(accountId)) {
+		return {};
+	}
+	const account = await getAccountById(String(accountId));
+
+	return account;
+};
